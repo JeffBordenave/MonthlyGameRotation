cf2fad2a2d6fb3c7368bccf66bf2960c6c6e0424
Geoffroy Bordenave* <gbordenave@artfx.fr>
2022-04-27 19:09:19 +0300

    WIP making walls spawn in circle

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject wallPrefab = default;
+
+    private void Start()
     {
-        
+        SpawnCircleOfWalls(10, 10);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void SpawnCircleOfWalls(int nOfWall, float radius)
     {
-        
+        for (int i = 0; i < nOfWall; i++)
+        {
+            Vector3 posVector = new Vector3(i * 10, 0, 0);
+            SpawnWall(posVector, Quaternion.identity);
+        }
+    }
+
+    private void SpawnWall(Vector3 position, Quaternion rotation)
+    {
+        GameObject wall = Instantiate(wallPrefab);
+        wall.transform.position = position;
+        wall.transform.rotation = rotation;
     }
 }
